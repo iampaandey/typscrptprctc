8f02b11f263be793e73704410492b49029f6efc3
Raghav Pandey <90841468+iampaandey@users.noreply.github.com>
2023-12-10 09:46:42 +0530

    first comit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import { Todo } from "../models/models"
+import SingleTodo from "./SingleTodo";
+import "./styles.css"
+interface Props {
+    todos:Todo[];
+    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList:React.FC<Props> = ({todos,setTodos}) => {
+  return ( 
+    <div className="todos">
+      {todos.map(todo=>(
+       <SingleTodo todo={todo} todos={todos} setTodos={setTodos}     />
+      ))}
+    </div>
+  )
+}
+
+export default TodoList
